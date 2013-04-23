7440aad8b07c0672e726c770ef5a5ae40cb075f7
Raphaël Khaïat <raphael@khaiat.org>
2013-04-23 13:43:39 +0200

    Clean the code by using the ng-class feature from angularjs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,41 +4,38 @@ angular.module('jocsApp')
   .controller('MainCtrl', function ($scope, $window, Comments) {
     $scope.comments = [];
     $scope.title = 'Jocs\' on me';
-    $scope.errorMessage = '';
-    $scope.status = 1;
-    $scope.statusMessage = 0;
+    $scope.isError = 0;
+    $scope.isSuccess = 0;
+    $scope.message = '';
     $scope.commentSent = false;
 
     var getCommentSuccess = function(data) {
       if (data.status === 'success') {
-        $scope.errorMessage = '';
         $scope.comments = data.comments;
-        $scope.status = 1;
       }
       else {
-        $scope.statusMessage = 0;
-        $scope.errorMessage = 'Comments for this page couldn\'t be retrieved';
-        $scope.status = 0;
+        $scope.isSuccess = 0;
+        $scope.isError = 1;
+        $scope.message = 'Comments for this page couldn\'t be retrieved';
       }
     };
 
     var getCommentError = function() {
-      $scope.statusMessage = 0;
-      $scope.errorMessage = 'An error occured';
-      $scope.status = 0;
+      $scope.isError = 1;
+      $scope.isSuccess = 0;
+      $scope.message = 'An error occured';
     };
 
     var addSuccess = function(data) {
       if (data.status === 'success') {
-        $scope.errorMessage = '';
-        $scope.status = 1;
-        $scope.statusMessage = data.message;
+        $scope.isError = 0;
+        $scope.isSuccess = 1;
+        $scope.message = data.message;
       }
       else {
-        $scope.statusMessage = 0;
-        $scope.errorMessage = data.message;
-        $scope.status = 0;
-        $scope.message = '';
+        $scope.message = data.message;
+        $scope.isSuccess = 0;
+        $scope.isError = 1;
       }
 
       $scope.commentSent = true;
@@ -46,9 +43,9 @@ angular.module('jocsApp')
     };
 
     var addError = function() {
-      $scope.errorMessage = 'Comment couldn\'t be added';
-      $scope.status = 0;
-      $scope.statusMessage = 0;
+      $scope.message = 'Comment couldn\'t be added';
+      $scope.isError = 1;
+      $scope.isSuccess = 0;
     };
 
     Comments.get(getCommentSuccess, getCommentError);
